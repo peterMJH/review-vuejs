2ca962ef5f43fa6ab28ae06e398649baf794707f
peterMoon <mjh9016@gmail.com>
2018-12-18 22:09:15 +0900

    add table Components, add Axios

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,6 +1,53 @@
 <template>
     <div>
-        Table
+        Checked Values : {{multiSelect}}
+        <table ref="multipleTable">
+            <tr>
+                <td><input type="checkbox" id="checkbox" @change="parentChecked"></td>
+                <td>id</td>
+                <td>userId</td>
+                <td>title</td>
+                <td>body</td>
+            </tr>
+            <tr v-for="(element, index) in data" v-bind:key="index">
+                <td><input type="checkbox" :value="index" v-model="multiSelect"></td>
+                <td>{{element.id}}</td>
+                <td>{{element.userId}}</td>
+                <td>{{element.title}}</td>
+                <td>{{element.body}}</td>
+            </tr>
+        </table>
     </div>
 </template>
 <script>
+export default {
+    data() {
+        return {
+            data: [],
+            multiSelect: [0, 1]
+        }
+    },
+    mounted() {
+        this.$http.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+            this.data = response.data
+        })
+    },
+    methods: {
+        parentChecked() {
+            alert('parentChecked')
+        }
+    }
+}
+</script>
+<style>
+  table {
+    width: 100%;
+    border: 1px solid #444444;
+    border-collapse: collapse;
+  }
+  th, td {
+    border: 1px solid #444444;
+    padding: 10px;
+  }
+</style>
+
